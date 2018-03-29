29f59420f9aa8a90af6f4f00ff2c6e6da7de1d86
Paul Han <p.han@yale.edu>
2018-03-29 18:25:00 -0400

    Update sketch_part2.ino

--- sketch_part2.ino
+++ sketch_part2.ino
@@ -27,26 +27,12 @@ void loop() {
   distance = time_value / 58; //calculate the distance in cm
  
   // **************the range 0 to 1023 may need to change!! just a placeholder for now!!!*******************
- 
-  output_voltage = constrain((6.5*(map(input_voltage, 0, 1023, 0, 255))),0,255);     
-  // We want to re-map the input voltage value to the range [0, 255] 
-  // We multiply by 6.5 to account for the low input voltage     
-  // Constrain is used to hard limit output values between 0 and 255  
-  
+  output_voltage = map(distance, 0, 1023, 0, 255); // Maps distance to an output analog voltage value in the range [0, 255]. The range [0, 1023] is a placeholder; may need to be changed
+
+
   Serial.print("Distance (cm): ");
   Serial.println(distance);
 
 
   Serial.print("Output voltage: ");      // for debugging; output the voltage value [0,255].
   Serial.println(output_voltage);
-
-  
-  
-
-  
-
-
-  analogWrite(red_pin, output_voltage);
-  delay(1000);                            //delay program by one second
-
-}
